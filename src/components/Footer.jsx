f3d1684fc74795b3ad6071afd4e2ffccee7197d4
Yancarlos Diaz <yxd3549@rit.edu>
2022-04-23 12:11:31 -0700

    Add images and reformat items in home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <footer className="py-3 bg-dark fixed-bottom">
         <div className="container">
           <p className="m-0 text-center text-white">
-            Copyright &copy; Your Website 2022
+            Copyright &copy; NTCountryCreations 2022
           </p>
         </div>
       </footer>
