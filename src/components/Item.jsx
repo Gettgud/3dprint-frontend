b32330de6aa7b81f27686d95745ffd8d2d2c36c0
Yancarlos Diaz <yxd3549@rit.edu>
2022-04-23 09:45:01 -0700

    Fix footer placement and added temporary item rendering on the home page

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import printer from "../images/printer.png"
 import PropTypes from "prop-types"
 
-function Item({name, img, description}) {
+function Item({name, img, imgAlt, description, itemWidth, }) {
   return (
-    <div className="card" style={{width: "400px"}}>
-        <img className="card-img-top" src={img} alt="image"/>
+    <div className="card" style={{width: itemWidth, marginBottom: "5px"}}>
+        <img className="card-img-top" src={img} alt={imgAlt}/>
         <div className="card-body">
         <h4 className="card-title">{name}</h4>
-        <p className="card-text">{description}</p>
+        { description !== null ? <p className="card-text">{description}</p> : null }
         </div>
     </div>
   );
@@ -18,7 +17,9 @@ Item.propTypes=
 {   
     name: PropTypes.string, 
     img: PropTypes.string, 
+    imgAlt: PropTypes.string,
     description: PropTypes.string,
+    itemWidth: PropTypes.string,
 }
 
 export default Item;
